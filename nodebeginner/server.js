cd998b56ac05f6c3213a10b876ef02c7137b034b
mactive <mactive@gmail.com>
2013-11-04 10:24:12 +0800

    version 1

--- nodebeginner/server.js
+++ nodebeginner/server.js
@@ -1,1 +1,9 @@
 var http= require("http");
+
+var server = http.createServer(function (request, response) {
+	response.writeHead(200, {"Content-type": "text/plain"});
+	response.write("<h1>Hello, Node.js!</h1>");
+	response.end();
+});
+
+server.listen(8888);
