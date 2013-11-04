36bc0ea1a583e2b22ec25a925fbc415b85fe90c9
mactive <mactive@gmail.com>
2013-11-04 10:31:56 +0800

    start.js

--- nodebeginner/server.js
+++ nodebeginner/server.js
@@ -1,9 +1,18 @@
 var http= require("http");
 
-var server = http.createServer(function (request, response) {
-	response.writeHead(200, {"Content-type": "text/plain"});
-	response.write("<h1>Hello, Node.js!</h1>");
-	response.end();
-});
 
-server.listen(8888);
+function start() {
+	function onRequest(request, response) {
+		// body...
+		console.log("Response received.");
+		response.writeHead(200, {"Content-type": "text/plain"});
+		response.write("<h1>Hello, Node.js!</h1>");
+		response.end();
+	}
+
+	http.createServer(onRequest).listen(8888);
+
+	console.log("Server has started");	
+}
+
+exports.start = start;
